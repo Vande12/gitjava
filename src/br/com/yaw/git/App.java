57283d23d512fb5083485908ce91e38e52de9740
vanderlei.121173 <vanderlei.121173@192.168.3.83>
2014-11-05 19:21:47 -0200

    Fazendo o commit inicial do projeto

---
+++ src/br/com/yaw/git/App.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.yaw.git;
+
+/**
+ *
+ * @author vanderlei.121173
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Utilizando o git atraves do NetBeans");
+    }
+}
