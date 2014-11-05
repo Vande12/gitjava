f223ea91731d45d006d6820a49c955320d29f307
vanderlei.121173 <vanderlei.121173@192.168.3.83>
2014-11-05 19:40:56 -0200

    Fazendo o commit inicial do projeto

---
+++ src/br/com/yaw/git/OutraClasse.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.yaw.git;
+
+/**
+ *
+ * @author vanderlei.121173
+ */
+public class OutraClasse {
+    
+}
