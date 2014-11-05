3f2a18df83c5d27076146c7be45b990640b413a2
Vande12 <vanderlei.machado@bento.ifrs.edu.br>
2014-11-05 19:54:55 -0200

    Update App.java
    web

--- src/br/com/yaw/git/App.java
+++ src/br/com/yaw/git/App.java
@@ -15,7 +15,7 @@ public class App {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Utilizando o git atraves do NetBeans 8.0");
+        System.out.println("Utilizando o git atraves do NetBeans 8.0 novo");
         int n=0;
     }
 }
